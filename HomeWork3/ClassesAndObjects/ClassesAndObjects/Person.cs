e2010a41c0263870a593c81abfb0a484e39cc3f1
JanisMauris <janis.mauris@gmail.com>
2022-04-24 11:34:39 +0300

    3rd HW

---
+++ HomeWork3/ClassesAndObjects/ClassesAndObjects/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Hobby { get; set; }
+        public bool Gender { get; set; }
+
+        public int GetAge()
+        {
+            return DateTime.Now.Year - BirthDate.Year;
+        }
+    }
+}
---
+++ HomeWork3/ClassesAndObjects/ClassesAndObjects/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    public class Product
+    {
+        public Product(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+        public double Length { get; }
+        public double Width { get; }
+        public double Height { get; }
+        public double Weight { get; set; }
+    }
+}
